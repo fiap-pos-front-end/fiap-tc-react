439d439bbcb40e8afe2043eb683fff2e878fb8cf
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-22 22:02:18 -0300

    feat: npmrc

--- server.cjs
+++ server.cjs
@@ -14,6 +14,7 @@ app.use(cors());
 
 app.use(express.static(clientDist));
 
+// TODO: tentar pegar a rota do shell e mostrar o sobre nos
 app.use((req, res) => {
   const appHtml = renderToString(React.createElement(NotFoundPage));
   const template = fs.readFileSync(path.join(clientDist, "index.html"), "utf8");
